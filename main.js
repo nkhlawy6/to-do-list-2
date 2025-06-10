53c2861faa47bc116a4a83068db57b283db702c0
emauser1 <emauser1@mci.ext>
2025-06-10 16:37:24 +0300

    created addTaskToArry addElementsToPageFrom

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
-let input=document.querySelector('.input');
-let submit=document.querySelector('.add');
-let tasksDiv=document.querySelector('.tasks')
+let input = document.querySelector(".input");
+let submit = document.querySelector(".add");
+let tasksDiv = document.querySelector(".tasks");
+
+//array to store data
+let arrayOfTasks = [];
+//Add Task
+submit.onclick = function () {
+  if (input.value != "") {
+    addTaskToArry(input.value);
+    input.value = "";
+  }
+};
+
+function addTaskToArry(taskText) {
+  //task data
+  let task = {
+    id: Date.now(),
+    title: taskText,
+    completed: false,
+  };
+  //push task to array to store it
+  arrayOfTasks.push(task);
+  //show tasks in the page
+  addElementsToPageFrom(arrayOfTasks);
+}
+
+function addElementsToPageFrom(arrayOfTasks) {
+  //empty tasks div
+  tasksDiv.innerHTML = "";
+  //looping on arrayOfTasks
+  arrayOfTasks.forEach((task) => {
+    //create main divtask
+    let divTask=document.createElement('div');
+    divTask.className='task';
+    if(task.completed){
+        divTask.className='task done'
+    }
+    divTask.setAttribute('data-id',task.id);
+    divTask.appendChild(document.createTextNode(task.title));
+    //create delete button
+    let span=document.createElement('span');
+    span.className='del';
+    span.appendChild(document.createTextNode('Delete'))
+    //append delete button to divTask
+    divTask.appendChild(span)
+    //append divTask to tasksDiv
+    tasksDiv.append(divTask)
+ 
+  });
+}
