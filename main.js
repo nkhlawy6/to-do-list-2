32c7408dc58bb240265c63c1077bf78662f36a95
emauser1 <emauser1@mci.ext>
2025-06-11 21:34:54 +0300

    addDataToLocalStorageFrom,getDataFromLocalStorage functions are completed

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ let tasksDiv = document.querySelector(".tasks");
 
 //array to store data
 let arrayOfTasks = [];
+
+//check if there's data in the localstorage
+if(window.localStorage.getItem('tasks')){
+  arrayOfTasks=JSON.parse(window.localStorage.getItem('tasks'));
+  console.log(arrayOfTasks)
+}
+
+//trigger data from localStorage
+getDataFromLocalStorage()
 //Add Task
 submit.onclick = function () {
   if (input.value != "") {
@@ -23,6 +32,7 @@ function addTaskToArry(taskText) {
   arrayOfTasks.push(task);
   //show tasks in the page
   addElementsToPageFrom(arrayOfTasks);
+  addDataToLocalStorageFrom(arrayOfTasks);
 }
 
 function addElementsToPageFrom(arrayOfTasks) {
@@ -31,21 +41,38 @@ function addElementsToPageFrom(arrayOfTasks) {
   //looping on arrayOfTasks
   arrayOfTasks.forEach((task) => {
     //create main divtask
-    let divTask=document.createElement('div');
-    divTask.className='task';
-    if(task.completed){
-        divTask.className='task done'
+    let divTask = document.createElement("div");
+    divTask.className = "task";
+    if (task.completed) {
+      divTask.className = "task done";
     }
-    divTask.setAttribute('data-id',task.id);
+    divTask.setAttribute("data-id", task.id);
     divTask.appendChild(document.createTextNode(task.title));
     //create delete button
-    let span=document.createElement('span');
-    span.className='del';
-    span.appendChild(document.createTextNode('Delete'))
+    let span = document.createElement("span");
+    span.className = "del";
+    span.appendChild(document.createTextNode("Delete"));
     //append delete button to divTask
-    divTask.appendChild(span)
+    divTask.appendChild(span);
     //append divTask to tasksDiv
-    tasksDiv.append(divTask)
- 
+    tasksDiv.append(divTask);
   });
+}
+
+
+//save data in localStorage
+function addDataToLocalStorageFrom(arrayOfTasks){
+window.localStorage.setItem('tasks',JSON.stringify(arrayOfTasks))
+}
+
+
+//get data from localStorage
+function getDataFromLocalStorage(){
+  let data=window.localStorage.getItem('tasks');
+  if(data){
+    let tasks=JSON.parse(data);
+    addElementsToPageFrom(tasks)
+   
+  }
+ 
 }
